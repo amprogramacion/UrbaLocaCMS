a32373beb08572f6feecb8cf7f81dd61199a6085
Angel <amprogramacion@gmail.com>
2023-03-25 03:04:04 +0100

    UL Old 2016 adaptado a php > 7.4 y mysqli

--- conectar.php
+++ conectar.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbhost = 'urba-loca.es';
+$dbhost = '82.223.54.179';
 $dbusername = 'urbaloca_old';
-$dbuserpass = '2k8x1Wa*8';
+$dbuserpass = "2k8x1Wa*8";
 $dbname = 'urbaloca_old_2016';
 $conn = mysqli_connect($dbhost, $dbusername, $dbuserpass, $dbname) or die('No se puede conectar a la base de datos');
 ?>
